f6929b0c4d4afb6bd493723f35564432c79e898f
Abraham Brookes <abrahambrookes@gmail.com>
2025-12-24 21:02:36 +1000

    add koyins to game UI

--- Scripts/Inventory/KoyinPouch.gd
+++ Scripts/Inventory/KoyinPouch.gd
@@ -1,15 +1,20 @@
 extends Node
 
+## The Koyin Pouch simply tracks our Koyins
 class_name KoyinPouch
 
-## The Koyin Pouch simply tracks our Koyins
-
+# the number of koyins
 @export var koyins : int = 0
+
+# the label in the UI where we show how many koyins we have
+@onready var label : Label = $"../InGameUI/Control/MarginContainer/HBoxContainer/MarginContainer/KoyinLabel"
 
 ## add koyins to the pouch
 func add_koyins(amount: int):
 	koyins += amount
+	label.text = str(koyins)
 	
 ## remove koyins from the pouch
 func remove_koyins(amount: int):
 	koyins -= amount
+	label.text = str(koyins)
