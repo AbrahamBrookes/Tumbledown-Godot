93a45f96b8bdaa4b47df1ec64e364bc9e71a04f6
Abraham Brookes <abrahambrookes@gmail.com>
2025-12-30 00:04:25 +1000

    update koyin - remove spin animation and give loot drop effect

--- Gameplay/Koyins/koyin.gd
+++ Gameplay/Koyins/koyin.gd
@@ -6,3 +6,9 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	if pouch:
 		pouch.add_koyins(1)
 		queue_free()
+
+# when we are loot dropped, go up and then settle back down
+func loot_dropped() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "position:y", position.y + 0.5, 0.6).set_trans(Tween.TransitionType.TRANS_SINE).set_ease(Tween.EaseType.EASE_OUT)
+	tween.tween_property(self, "position:y", position.y, 1.5).set_delay(0.3).set_trans(Tween.TransitionType.TRANS_SINE).set_ease(Tween.EaseType.EASE_IN)
