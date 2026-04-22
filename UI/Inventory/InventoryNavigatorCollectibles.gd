997b90897eed15134ae408f658b7d731cf925768
Abraham Brookes <abrahambrookes@gmail.com>
2026-04-23 00:57:34 +1000

    working on inventory navigation

---
+++ UI/Inventory/InventoryNavigatorCollectibles.gd
@@ -0,0 +1,116 @@
+extends Node
+
+## This node exists as a component on the InventoryUI to capture input and allow the user to navigate
+## and interact with the inventory. The inventory screen is made up of a couple of different screens
+## each for different things - ie one for collectibles, one for quest items, one for usable items.
+## This script in particular is just for the collectibles screen which is a GridContainer.
+## We track the location of the cursor and react to the same input that the player uses to walk in
+## game but instead we move the highlight and "hover" an item.
+
+class_name InventoryNavigatorCollectibles
+
+## Emitted when the cursor exceeds the right of the box
+signal exit_right
+## Emitted when the cursor exceeds the left of the box
+signal exit_left
+## Emitted when the cursor exceeds the top of the box
+signal exit_top
+## Emitted when the cursor exceeds the bottom of the box
+signal exit_bottom
+
+## A reference to the grid container rendering our collectibles
+@export var grid: GridContainer
+
+## Our GridContainer has a defined size width and height, so we need to track the currently hovered
+## coordinates in a vec2. NOTE we are assuming 0, 0 is top left because the index in the gridcontainer
+## starts at 0
+var current_location: Vector2i = Vector2i(0, 0)
+
+## We need to track the height and width of the GridContainer - this can also be used to set the grid
+## dimensions if we ever want to do that I guess
+var grid_height : int = 4
+var grid_width: int = 10
+
+## we only want to render the cursor if we are active
+var should_render_cursor: bool = true
+
+## Listen to input for the sake of navigating
+func _unhandled_input(event: InputEvent) -> void:
+	# bail out early if we're not supposed to be rendering
+	if not should_render_cursor:
+		return
+	
+	if event.is_action_pressed("walk_east"):
+		print("east")
+		if current_location.x == grid_width - 1 or get_current_index() == grid.get_child_count() - 1:
+			#should_render_cursor = false
+			exit_right.emit()
+		else:
+			current_location.x = current_location.x + 1
+		render_cursor()
+		
+	if event.is_action_pressed("walk_west"):
+		print("west")
+		if current_location.x == 0:
+			#should_render_cursor = false
+			exit_left.emit()
+		else:
+			current_location.x = current_location.x - 1
+		render_cursor()
+		
+	if event.is_action_pressed("walk_north"):
+		print("north")
+		if current_location.y == 0:
+			#should_render_cursor = false
+			exit_top.emit()
+		else:
+			current_location.y = current_location.y - 1
+		render_cursor()
+		
+	if event.is_action_pressed("walk_south"):
+		print("south")
+		if current_location.y == grid_height - 1 or get_current_index() == grid.get_child_count() - 1:
+			#should_render_cursor = false
+			exit_bottom.emit()
+		else:
+			current_location.y = current_location.y + 1
+		render_cursor()
+	
+
+## Rendering the cursor displays where we are currently hovering
+func render_cursor():
+	get_viewport().set_input_as_handled()
+	
+	# un-highlight all ItemSlots in the grid
+	for child in grid.get_children():
+		if child is ItemSlot:
+			child.highlight(false)
+		
+	# bail out early if we're not supposed to be rendering
+	if not should_render_cursor:
+		return
+		
+	var current_index = get_current_index()
+	
+	# get the slot there
+	var slot = grid.get_child(current_index) as ItemSlot
+	print("current_index %d, grid_width %d, %v" % [current_index, grid_width, current_location])
+	print(grid.get_child_count())
+	## we are expecting the slot to be an ItemSlot
+	if slot == null:
+		return
+	
+	# highlight the slot
+	slot.highlight(true)
+	
+## A helpful getter for our current index
+func get_current_index() -> int:
+	# gridcontainers don't have knowledge of x and y they just store a list of slots so we need to
+	# convert from our x and y to a total, which is columns * x + y (*x because the grid is horizontal
+	# in this instance)
+	var index = (grid.columns * current_location.y) + current_location.x
+	
+	# clamp to the grid max so we don't overflow the index
+	index = clamp(index, 0, grid.get_child_count() - 1)
+	
+	return index
--- UI/Inventory/ItemSlot.gd
+++ UI/Inventory/ItemSlot.gd
@@ -15,6 +15,18 @@ var quantity: int
 ## The label we print the quantity to
 @export var qty_label: Label
 
+## The PanelContainer that provides the box styling, for highlights
+@export var panel_box: PanelContainer
+
+## In order to show a highlight we want a non-highlighted and a highlighted style theme
+## that we swap out as the user navigates item slots
+
+## The style to use when the slot is highlighted
+@export var style_highlight: StyleBoxFlat
+
+## The style to use when the slot is not highlighted
+@export var style_idle: StyleBoxFlat
+
 ## Rendering the slot means grabbing the image and slapping it in the texture rect
 func render():
 	image.texture = item_data.icon
@@ -25,3 +37,10 @@ func set_item(item: ItemData, qty: int):
 	item_data = item
 	quantity = qty
 	render()
+
+## When the user is hovering the slot, highlight it. Pass a bool to toggle
+func highlight(enabled: bool):
+	panel_box.add_theme_stylebox_override(
+		"panel",
+		style_highlight if enabled else style_idle
+	)
